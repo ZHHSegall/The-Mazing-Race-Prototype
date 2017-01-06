993d35b066610533493608d581c9db6b6fae9329
Zachary Segall <zhhsegall@gmail.com>
2017-01-05 21:19:26 -0700

    Add movement

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,4 +1,7 @@
 package controller;
+import java.io.InputStream;
+import java.util.Scanner;
+
 import models.*;
 
 public class Main {
@@ -7,8 +10,35 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Maze gameboard = new Maze(STANDARD_BOARD_SIZE, STANDARD_BOARD_SIZE);
+		Scanner in = new Scanner(System.in);
 		
-		gameboard.print();
+		while(gameboard.gameOver() == 0){
+			gameboard.print();
+			System.out.println();
+			gameboard.move(getMove(in));
+			System.out.println();
+		}
+		if(gameboard.gameOver() == 1){
+			System.out.println("PLAYER 1 WINS!" );
+		} else {
+			System.out.println("PLAYER 2 WINS!" );
+		}
+		in.close();
 	}
-
+	
+	
+	public static char getMove(Scanner in){
+		boolean valid = false;
+		char direction = 'c';
+		while(!valid){
+			System.out.println("Please enter a direction to move: (l)eft, (r)ight, (u)p, (d)own");
+			//TODO Improve character verification
+			direction = in.nextLine().toLowerCase().charAt(0);
+			valid = (direction == 'u' || direction == 'd' || direction == 'l' || direction == 'r');
+			if(!valid){
+				System.out.println("Not a valid direction. Please try again");
+			}
+		}
+		return direction;
+	}
 }
