628e91c654efd166ae96c8acc39c9cedcfbda68c
Zachary Segall <zhhsegall@gmail.com>
2017-01-06 19:57:00 -0700

    Add functional (but messy) A* search for move verification

--- src/models/PathFinder.java
+++ src/models/PathFinder.java
@@ -3,118 +3,108 @@ package models;
 import java.util.*;
 
 public class PathFinder {
-	private class Node {
-		Node parent;
-		Coordinates loc;
-		float f, g, h;
-		
-		protected Node (Node parent, Coordinates loc, float h){
-			this.parent = parent;
-			this.loc = loc;
-			this.h = h;
-			this.g = (parent == null) ? 0 : parent.g + 1;
-			this.f = this.h + this.g;
-		}
-		
-		protected boolean at(Coordinates other){
-			return loc.equals(other);
-		}
-		
-		protected boolean at(Node other){
-			return loc.equals(other.loc);
-		}
-	}
 	
 	//Uses A* search to test if there is a path between two points.
 	
 	Maze board;
 	Coordinates start;
 	Coordinates finish;
-	List<Node> frontier; //Open list - ordered wrt smallest f
-	HashMap<Coordinates, Node> frontierMap;
-	HashMap<Coordinates, Node> explored; //Closed list - ordered wrt smallest f
+	Coordinates[][] squares;
+	Set<Coordinates> frontier;
+	Set<Coordinates> explored; //Closed list - ordered wrt smallest f
+	HashMap<Coordinates, Integer> gScores;
+	HashMap<Coordinates, Integer> fScores;
 	static char[] DIRECTIONS = {'u', 'r', 'd', 'l'};
 	
 	public PathFinder(Maze b, Coordinates s, Coordinates f){
 		board = b;
-		start = s;
+		squares = new Coordinates[board.height][board.width];
+		for(int row = 0; row < board.height; row++){
+			for(int col = 0; col < board.width; col++){
+				squares[row][col] = new Coordinates(col, row);
+			}
+		}
+			
+		start = squares[s.y][s.x];
 		finish = f;
-		frontier = new ArrayList<Node>();
-		frontierMap = new HashMap<Coordinates, Node>();
-		explored = new HashMap<Coordinates, Node>();
-		frontier.add(new Node(null, s, 0));
-		frontierMap.put(s, frontier.get(0));
+		frontier = new HashSet<Coordinates>();
+		explored = new HashSet<Coordinates>();
+		gScores = new HashMap<Coordinates, Integer>();
+		fScores = new HashMap<Coordinates, Integer>();
+		
+		//Add first node
+		frontier.add(start);
+		gScores.put(start, 0);
+		fScores.put(start, start.manDistanceTo(finish));
 	}
 	
-
-	public static void insertNode(List<Node> lst, Node n){
-		if(lst.size() == 0){
-			lst.add(n);
-		} else {
-			//Binary insertion search
-			int low = 0, high = lst.size();
-			int mid = (high - low)/2 + low;
-			while(high > low){
-				if(n.f > lst.get(mid).f){
-					low = mid + 1;
-				} else if (n.f <= lst.get(mid).f){
-					high = mid - 1;
-				}
-				mid = (high - low)/2 + low;
+	public Coordinates getBestF (){
+		double bestF = Double.POSITIVE_INFINITY;
+		Coordinates best = null;
+		for(Coordinates cur : frontier){
+			int curF = fScores.get(cur); 
+			if(curF < bestF){
+				best = cur;
+				bestF = curF;
 			}
-			lst.add(low, n);
 		}
+		return best;
 	}
 	
 	//Returns null if the proposed successor is invalid
 	//Uses Manhattan distance for heuristic
-	private Node genSuccessor(Node cur, char dir){
-		Node succ = null;
-		Coordinates succLoc = new Coordinates(0,0); 
-		if(board.moveIsValid(dir, cur.loc)){
+	private Coordinates genSuccessor(Coordinates cur, char dir){
+		if(board.moveIsValid(dir, cur)){
 			switch(dir){
 			case 'u': 
-				succLoc = new Coordinates(cur.loc.x, cur.loc.y + 1);
-				break;
+				return squares[cur.y + 1][cur.x];
 			case 'r':
-				succLoc = new Coordinates(cur.loc.x + 1, cur.loc.y);
-				break;
+				return squares[cur.y ][cur.x + 1];
 			case 'd': 
-				succLoc = new Coordinates(cur.loc.x, cur.loc.y - 1);
-				break;
+				return squares[cur.y - 1][cur.x];
 			case 'l':
-				succLoc = new Coordinates(cur.loc.x - 1, cur.loc.y);
-				break;
+				return squares[cur.y][cur.x - 1];
 			}
-			succ = new Node(cur, succLoc, succLoc.manDistanceTo(finish));
 		}
-		return succ;
+		return null;
 	}
 	
 	public boolean findPath(){
-		while(frontier.size() > 0){
-			//Removes current node from frontier
-			Node cur = frontier.remove(0);
-			frontierMap.remove(cur.loc);
-			//Add current node to explored list
-			explored.put(cur.loc, cur);
+		while(!frontier.isEmpty()){
+			//If current node is finish, end
+			Coordinates cur = getBestF();
+			if(cur.equals(finish)) 
+				return true;
 			
-			System.out.println("Cur Coordinates: (" + cur.loc.x + ", " + cur.loc.y + "), f = " + cur.f 
-					+ " g = " + cur.g + " h = " + cur.h);
+			//Removes current node from frontier
+			frontier.remove(cur);
+			//Add current node to explored
+			explored.add(cur);
+			
+			/*
+			for(Coordinates c : frontier.keySet()){
+				System.out.print("(" + c.x + ", " + c.y + ") -");
+			}
+			System.out.println();*/
+			//System.out.println("Cur Coordinates: (" + cur.loc.x + ", " + cur.loc.y + "), f = " + cur.f 
+			//		+ " g = " + cur.g + " h = " + cur.h);
 			for(char dir : DIRECTIONS){
-				Node succ = this.genSuccessor(cur, dir);
-				if(succ != null){
-					if(succ.at(finish)) 
-						return true;
+				Coordinates succ = this.genSuccessor(cur, dir);
+				if(succ != null && !explored.contains(succ)){
 					
-					//If a node with the same location is in the frontier or explored list, skip
-					if(!(frontierMap.containsKey(succ.loc) || explored.containsKey(succ.loc))){ 
-						insertNode(frontier, succ); 
-						frontierMap.put(succ.loc, succ);
+					boolean skip = false;
+					int succG = gScores.get(cur) + 1;
+					if(!frontier.contains(succ)){
+						frontier.add(succ);
+					} else if (succG >= gScores.get(succ)) {
+						skip = true;
+					}
+					if(!skip){
+						gScores.put(succ, succG);
+						fScores.put(succ, succG + succ.manDistanceTo(finish));
 					}
 				}
 			}
-			
 		}
 	
 		return false;
